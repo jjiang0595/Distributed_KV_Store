915f86f3ecc6691b5ff3fda1539737abb9c1f24f
Jerry Jiang <jerryjiang49@gmail.com>
2025-05-15 00:02:05 -0400

    feat: add PUT and GET handlers with Node storage
    - Add HTTP PUT and GET endpoints for the single-node KV store
    - Setup sample cluster nodes for testing

---
+++ cmd/server/main.go
@@ -0,0 +1,93 @@
+package server
+
+import (
+	"distributed_kv_store/internal/cluster/types"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+)
+
+var tempStore = make(map[string][]byte)
+var mu sync.Mutex
+
+type Config struct {
+	Node struct {
+		ID      string `yaml:"id"`
+		Address string `yaml:"address"`
+		Port    string `yaml:"port"`
+	} `yaml:"node"`
+	Cluster struct {
+		Peers []types.Node `yaml:"peers"`
+	} `yaml:"cluster"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	yamlFile, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func main() {
+	cfg, err := LoadConfig("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	port, err := strconv.Atoi(cfg.Node.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	node := &types.Node{
+		ID:      cfg.Node.ID,
+		Address: cfg.Node.Address,
+		Port:    port,
+		Data:    make(map[string][]byte),
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPut {
+			key := r.URL.Path[1:]
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			defer r.Body.Close()
+			node.Put(key, body)
+			w.WriteHeader(http.StatusOK)
+			_, printErr := fmt.Fprintf(w, "PUT successful for key: %s\n", key)
+			if printErr != nil {
+				log.Printf("Error writing response: %s\n", printErr)
+			}
+		} else if r.Method == http.MethodGet {
+			key := r.URL.Path[1:]
+			value, err := node.Get(key)
+			if err != nil {
+				http.Error(w, "key not found", http.StatusNotFound)
+				return
+			}
+			_, writeErr := w.Write(value)
+			if writeErr != nil {
+				return
+			}
+		}
+	})
+}
