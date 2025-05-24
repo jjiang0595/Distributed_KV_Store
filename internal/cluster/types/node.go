d58acbaefd7dd0bcc1a0289b114fea6168e35bcb
Jerry Jiang <jerryjiang49@gmail.com>
2025-05-23 23:18:50 -0400

    feat: add additional fields in type Node for raft

--- internal/cluster/types/node.go
+++ internal/cluster/types/node.go
@@ -3,6 +3,15 @@ package types
 import (
 	"fmt"
 	"sync"
+	"time"
+)
+
+type RaftState string
+
+const (
+	Follower  RaftState = "follower"
+	Candidate RaftState = "candidate"
+	Leader    RaftState = "leader"
 )
 
 type Node struct {
@@ -10,12 +19,35 @@ type Node struct {
 	Address  string `yaml:"address"`
 	Port     int    `yaml:"port"`
 	GrpcPort int    `yaml:"grpc_port"`
-	Data     map[string][]byte
-	Mu       sync.Mutex
+
+	// KV Store Data
+	Data map[string][]byte
+	Mu   sync.Mutex
+
+	// Raft
+	RaftMu sync.Mutex
+
+	CurrentTerm      uint64            // Latest term server
+	VotedFor         string            // Candidate ID that the node voted for
+	Log              []LogEntry        // Replicated messages log
+	CommitIndex      uint64            // Index of highest log entry to be committed
+	State            RaftState         // Leader, Candidate, Follower
+	LeaderID         string            // Default "" if not leader, node ID otherwise
+	VotesReceived    map[string]bool   // Set of node IDs that stores whether the node voted for the current candidate
+	NextIndex        map[string]uint64 // Follower's next log entry's index
+	MatchIndex       map[string]uint64 // Follower's index of the highest log entry to be replicated
+	ElectionTimeout  *time.Timer       // Timer for election timeouts
+	HeartbeatTimeout *time.Timer       // Timer for leader heartbeats
 }
 
 type NodeMap struct {
 	Nodes []Node
+}
+
+type LogEntry struct {
+	Index   uint64
+	Term    uint64
+	Command []byte
 }
 
 func (n *Node) Put(key string, value []byte) {
