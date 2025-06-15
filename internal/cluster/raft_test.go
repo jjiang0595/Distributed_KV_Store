2af4c201eff67198d1222510d4f1edceb91c1dcf
Jerry Jiang <jerryjiang49@gmail.com>
2025-06-15 18:09:19 -0400

    feat: add single leader & leader crash recovery tests

--- internal/cluster/raft_test.go
+++ internal/cluster/raft_test.go
@@ -44,3 +44,104 @@ func filterSelfID(nodeID string, nodes []string) []string {
 	}
 	return filteredList
 }
+
+func TestLeaderElection_SingleLeader(t *testing.T) {
+	_, cancel, testNodes, clk := testSetup(t)
+	defer cancel()
+
+	clk.Advance(3 * time.Second)
+	timeout := 500 * time.Millisecond
+	startTime := time.Now()
+
+	leaderCount := false
+	for time.Since(startTime) < timeout {
+		for _, node := range testNodes {
+			if node.State == Leader {
+				leaderCount = true
+				break
+			}
+		}
+		clk.Advance(3 * time.Millisecond)
+		time.Sleep(3 * time.Millisecond)
+	}
+
+	if leaderCount {
+		t.Logf("Success: Single Leader")
+	} else {
+		t.Fatalf("followerCount should be 2")
+	}
+
+	t.Cleanup(func() {
+		for _, node := range testNodes {
+			go node.Shutdown()
+		}
+		for _, node := range testNodes {
+			clk.Advance(1 * time.Second)
+			node.WaitAllGoroutines()
+		}
+	})
+	t.Logf("Finish")
+}
+
+func TestLeaderElection_LeaderCrashRecovery(t *testing.T) {
+	_, cancel, testNodes, clk := testSetup(t)
+	defer cancel()
+	newCtx, newCancel := context.WithCancel(context.Background())
+	defer newCancel()
+
+	clk.Advance(3 * time.Second)
+	startTime := time.Now()
+	timeout := 500 * time.Millisecond
+
+	for time.Since(startTime) < timeout {
+		for _, node := range testNodes {
+			if node.State == Leader {
+				log.Printf("--------------------------------------CRASHING LEADER NODE------------------------------------------------")
+				crashedNode := testNodes[node.ID]
+				delete(testNodes, node.ID)
+				node.Cancel()
+				go node.Shutdown()
+				clk.Advance(1 * time.Second)
+				node.WaitAllGoroutines()
+				mockTransport := NewMockNetworkTransport(node.ID, testNodes)
+				testNodes[node.ID] = newTestNode(t, newCtx, newCancel, crashedNode.ID, crashedNode.Address, crashedNode.Port, crashedNode.GrpcPort, crashedNode.DataDir, crashedNode.Peers, crashedNode.Clock, mockTransport)
+				break
+			}
+		}
+		clk.Advance(3 * time.Millisecond)
+		time.Sleep(3 * time.Millisecond)
+	}
+
+	clk.Advance(3 * time.Second)
+	startTime = time.Now()
+	timeout = 500 * time.Millisecond
+
+	leaderCount := false
+	for time.Since(startTime) < timeout {
+		for _, node := range testNodes {
+			if node.State == Leader {
+				leaderCount = true
+				break
+			}
+		}
+		clk.Advance(3 * time.Millisecond)
+		time.Sleep(3 * time.Millisecond)
+	}
+
+	if leaderCount {
+		t.Logf("Success: New leader found after leader node crashed")
+	} else {
+		t.Fatalf("Error: New node not found after leader node crashed")
+	}
+	t.Cleanup(func() {
+		for _, node := range testNodes {
+			log.Printf("%s", node.ID)
+			go node.Shutdown()
+		}
+		for _, node := range testNodes {
+			clk.Advance(5 * time.Second)
+			node.WaitAllGoroutines()
+		}
+	})
+	t.Logf("Finish")
+}
