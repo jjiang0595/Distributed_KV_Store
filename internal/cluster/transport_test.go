d113a6da93b1b0a0911514d32bce53c2c75460cf
Jerry Jiang <jerryjiang49@gmail.com>
2025-06-21 18:36:20 -0400

    refactor: add context to network transport methods

--- internal/cluster/transport_test.go
+++ internal/cluster/transport_test.go
@@ -6,39 +6,41 @@ import (
 )
 
 type MockNetworkTransport struct {
+	ctx       context.Context
 	nodeID    string
 	peerNodes map[string]*Node
 }
 
-func NewMockNetworkTransport(nodeID string, peerNodes map[string]*Node) NetworkTransport {
+func NewMockNetworkTransport(ctx context.Context, nodeID string, peerNodes map[string]*Node) NetworkTransport {
 	return &MockNetworkTransport{
+		ctx:       ctx,
 		nodeID:    nodeID,
 		peerNodes: peerNodes,
 	}
 }
 
-func (m *MockNetworkTransport) SendAppendEntries(peerID string, req *AppendEntriesRequest) (*AppendEntriesResponse, error) {
+func (m *MockNetworkTransport) SendAppendEntries(appendCtx context.Context, peerID string, req *AppendEntriesRequest) (*AppendEntriesResponse, error) {
 	peer, ok := m.peerNodes[peerID]
 	if !ok {
 		return nil, fmt.Errorf("peer not found %s", peerID)
 	}
 
 	mockServer := NewRaftServer(peer)
-	res, err := mockServer.ProcessAppendEntriesRequest(context.Background(), req)
+	res, err := mockServer.AppendEntries(appendCtx, req)
 	if err != nil {
 		return nil, err
 	}
 	return res, nil
 }
 
-func (m *MockNetworkTransport) SendRequestVote(peerID string, req *RequestVoteRequest) (*RequestVoteResponse, error) {
+func (m *MockNetworkTransport) SendRequestVote(requestCtx context.Context, peerID string, req *RequestVoteRequest) (*RequestVoteResponse, error) {
 	peer, ok := m.peerNodes[peerID]
 	if !ok {
 		return nil, fmt.Errorf("peer not found %s", peerID)
 	}
 
 	mockServer := NewRaftServer(peer)
-	res, err := mockServer.ProcessVoteRequest(context.Background(), req)
+	res, err := mockServer.RequestVote(requestCtx, req)
 	if err != nil {
 		return nil, err
 	}
