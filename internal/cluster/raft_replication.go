d2df5167a293ee2e9dea8e68834c32d899dcef39
Jerry Jiang <jerryjiang49@gmail.com>
2025-06-23 16:41:39 -0400

    feat: offload leader replication process

--- internal/cluster/raft_replication.go
+++ internal/cluster/raft_replication.go
@@ -29,7 +29,77 @@ func (n *Node) StopReplicators() {
 	n.replicatorCancel = nil
 }
 
-func (n *Node) ReplicateToFollower(stopCtx context.Context, followerID string) {
+func (n *Node) sendAppendEntriesToPeers(stopCtx context.Context, followerID string) (bool, error) {
+	n.RaftMu.Lock()
+	term, leaderID, commitIndex := n.currentTerm, n.leaderID, n.commitIndex
+	logSnapshot := make([]*LogEntry, 0, len(n.log))
+	for _, entry := range n.log {
+		if entry != nil {
+			logEntry := &LogEntry{
+				Term:    entry.Term,
+				Index:   entry.Index,
+				Command: make([]byte, len(entry.Command)),
+			}
+			copy(logEntry.Command, entry.Command)
+			logSnapshot = append(logSnapshot, logEntry)
+		}
+	}
+	peerNextIndex, ok := n.nextIndex[followerID]
+	if !ok {
+		peerNextIndex = 1
+	}
+	n.RaftMu.Unlock()
+	prevLogIndex, prevLogTerm := uint64(0), uint64(0)
+	if peerNextIndex > 1 {
+		if peerNextIndex-2 < uint64(len(logSnapshot)) {
+			prevLogIndex = peerNextIndex - 1
+			prevLogTerm = logSnapshot[peerNextIndex-2].Term
+		} else {
+			log.Print("Indexes out of bound. Resetting to prevLogIndex & prevLogTerm to 0")
+			prevLogIndex = 0
+			prevLogTerm = 0
+		}
+	}
+
+	entries := make([]*LogEntry, 0)
+
+	if peerNextIndex > 0 && peerNextIndex <= uint64(len(logSnapshot)) {
+		entries = logSnapshot[peerNextIndex-1:]
+	} else if len(logSnapshot) > 0 && peerNextIndex == 0 {
+		entries = logSnapshot
+	} else if len(logSnapshot) > 0 && peerNextIndex > uint64(len(logSnapshot)) {
+		entries = []*LogEntry{}
+	}
+	replicateCtx, replicateCancel := context.WithTimeout(stopCtx, 200*time.Millisecond)
+	//log.Printf("Leader %s: ReplicateToFollower sending logs to %v at time %v", leaderID, followerID, n.Clock.Now())
+	response, err := n.Transport.SendAppendEntries(replicateCtx, followerID, &AppendEntriesRequest{
+		Term:         term,
+		LeaderId:     leaderID,
+		PrevLogIndex: prevLogIndex,
+		PrevLogTerm:  prevLogTerm,
+		Entries:      entries,
+		LeaderCommit: commitIndex,
+	})
+	replicateCancel()
+	if err != nil {
+		return false, nil
+	}
+	wrappedResp := &AppendEntriesResponseWrapper{
+		Response:     response,
+		Error:        err,
+		PeerID:       followerID,
+		PrevLogIndex: prevLogIndex,
+		SentEntries:  entries,
+	}
+	select {
+	case <-stopCtx.Done():
+		return response.GetSuccess(), nil
+	case n.appendEntriesResponseChan <- wrappedResp:
+		log.Printf("Leader %v: Processing Replication Response", n.ID)
+	}
+	return response.GetSuccess(), nil
+}
+
 	defer func() {
 		n.replicatorWg.Done()
 		log.Printf("Node %s: Replicator goroutine stopped", n.ID)
@@ -42,77 +112,10 @@ func (n *Node) ReplicateToFollower(stopCtx context.Context, followerID string) {
 		case <-n.ctx.Done():
 			log.Printf("Leader %s: ReplicateToFollower stopped", n.ID)
 			return
-		default:
-			n.RaftMu.Lock()
-			if n.state != Leader {
-				n.RaftMu.Unlock()
-				return
-			}
-			term, leaderID, commitIndex := n.currentTerm, n.leaderID, n.commitIndex
-			logSnapshot := make([]*LogEntry, len(n.log))
-			copy(logSnapshot, n.log)
-			peerNextIndex, ok := n.nextIndex[followerID]
-			if !ok {
-				peerNextIndex = 1
-			}
-			n.RaftMu.Unlock()
-			prevLogIndex, prevLogTerm := uint64(0), uint64(0)
-			if peerNextIndex > 1 {
-				if peerNextIndex-2 < uint64(len(logSnapshot)) {
-					prevLogIndex = peerNextIndex - 1
-					prevLogTerm = logSnapshot[peerNextIndex-2].Term
-				} else {
-					log.Print("Indexes out of bound. Resetting to prevLogIndex & prevLogTerm to 0")
-					prevLogIndex = 0
-					prevLogTerm = 0
-				}
-			}
 
-			entries := make([]*LogEntry, 0)
-
-			if peerNextIndex > 0 && peerNextIndex <= uint64(len(logSnapshot)) {
-				entries = logSnapshot[peerNextIndex-1:]
-			} else if len(logSnapshot) > 0 && peerNextIndex == 0 {
-				entries = logSnapshot
-			} else if len(logSnapshot) > 0 && peerNextIndex > uint64(len(logSnapshot)) {
-				entries = []*LogEntry{}
 			}
-
-			log.Printf("Leader %s: ReplicateToFollower sending logs to %v", leaderID, followerID)
-			response, err := n.Transport.SendAppendEntries(followerID, &AppendEntriesRequest{
-				Term:         term,
-				LeaderId:     leaderID,
-				PrevLogIndex: prevLogIndex,
-				PrevLogTerm:  prevLogTerm,
-				Entries:      entries,
-				LeaderCommit: commitIndex,
-			})
-			if err != nil {
-				log.Printf("Error appending raft log: %v", err)
 				retryTime = min(maxRetryTime, retryTime*2)
-				n.Clock.Sleep(retryTime)
-				continue
 			}
-			wrappedResp := &AppendEntriesResponseWrapper{
-				Response:     response,
-				Error:        err,
-				PeerID:       followerID,
-				PrevLogIndex: prevLogIndex,
-				SentEntries:  entries,
-			}
-			n.appendEntriesResponseChan <- wrappedResp
-
-			retryTime = 50 * time.Millisecond
-			sleepDuration := 10 * time.Millisecond
-			if response.Success {
-				sleepDuration = 50 * time.Millisecond
-			}
-			n.RaftMu.Lock()
-			log.Printf("MatchIndex: %v, nextIndex: %v", n.matchIndex[followerID], n.nextIndex[followerID])
-			log.Printf("log: %v", n.log)
-			log.Printf("Leader %s: Sleep %v", n.ID, sleepDuration)
-			n.RaftMu.Unlock()
-			n.Clock.Sleep(sleepDuration)
 		}
 	}
 }
