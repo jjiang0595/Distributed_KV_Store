f2fc333dd2eb8551dc1d355d002e77e48baf9e56
Jerry Jiang <jerryjiang49@gmail.com>
2025-06-25 00:08:57 -0400

    feat: add mock network transport for tests
    - add partitioned nodes to simulate network partition

--- internal/cluster/mock_transport.go
+++ internal/cluster/mock_transport.go
@@ -3,23 +3,46 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 type MockNetworkTransport struct {
 	ctx       context.Context
 	nodeID    string
 	peerNodes map[string]*Node
+
+	mu               sync.Mutex
+	partitionedNodes map[string]struct{}
 }
 
 func NewMockNetworkTransport(ctx context.Context, nodeID string, peerNodes map[string]*Node) NetworkTransport {
 	return &MockNetworkTransport{
-		ctx:       ctx,
-		nodeID:    nodeID,
-		peerNodes: peerNodes,
+		ctx:              ctx,
+		nodeID:           nodeID,
+		peerNodes:        peerNodes,
+		partitionedNodes: make(map[string]struct{}),
+	}
+}
+
+func (m *MockNetworkTransport) PartitionNode(nodeID string, isPartitioned bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if isPartitioned {
+		m.partitionedNodes[nodeID] = struct{}{}
+	} else {
+		delete(m.partitionedNodes, nodeID)
 	}
 }
 
 func (m *MockNetworkTransport) SendAppendEntries(appendCtx context.Context, peerID string, req *AppendEntriesRequest) (*AppendEntriesResponse, error) {
+	m.mu.Lock()
+	_, isPartitioned := m.partitionedNodes[peerID]
+	m.mu.Unlock()
+
+	if isPartitioned {
+		return nil, fmt.Errorf("node %s is partitioned", peerID)
+	}
 	peer, ok := m.peerNodes[peerID]
 	if !ok {
 		return nil, fmt.Errorf("peer not found %s", peerID)
@@ -27,6 +50,7 @@ func (m *MockNetworkTransport) SendAppendEntries(appendCtx context.Context, peer
 
 	mockServer := NewRaftServer(peer)
 	res, err := mockServer.AppendEntries(appendCtx, req)
+	runtime.Gosched()
 	if err != nil {
 		return nil, err
 	}
@@ -34,6 +58,13 @@ func (m *MockNetworkTransport) SendAppendEntries(appendCtx context.Context, peer
 }
 
 func (m *MockNetworkTransport) SendRequestVote(requestCtx context.Context, peerID string, req *RequestVoteRequest) (*RequestVoteResponse, error) {
+	m.mu.Lock()
+	_, isPartitioned := m.partitionedNodes[peerID]
+	m.mu.Unlock()
+
+	if isPartitioned {
+		return nil, fmt.Errorf("node %s is partitioned", peerID)
+	}
 	peer, ok := m.peerNodes[peerID]
 	if !ok {
 		return nil, fmt.Errorf("peer not found %s", peerID)
@@ -41,6 +72,7 @@ func (m *MockNetworkTransport) SendRequestVote(requestCtx context.Context, peerI
 
 	mockServer := NewRaftServer(peer)
 	res, err := mockServer.RequestVote(requestCtx, req)
+	runtime.Gosched()
 	if err != nil {
 		return nil, err
 	}
