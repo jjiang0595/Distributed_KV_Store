39d3d7e2c6dd8fd8cff4f3bc62b57683a23adaf7
Jerry Jiang <jerryjiang49@gmail.com>
2025-07-25 00:17:13 -0400

    feat: implement koanf to load config file/env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,19 +7,25 @@ import (
 	"flag"
 	"fmt"
 	"github.com/jonboulle/clockwork"
-	"github.com/spf13/viper"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env/v2"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 )
+
+var k = koanf.New(".")
 
 type Config struct {
 	Node struct {
 		ID       string `mapstructure:"id" yaml:"id"`
 		Address  string `mapstructure:"address" yaml:"address"`
-		Port     int    `mapstructure:"port" yaml:"port"`
+		Port     int    `mapstructure:"http_port" yaml:"http_port"`
 		GrpcPort int    `mapstructure:"grpc_port" yaml:"grpc_port"`
 		DataDir  string `mapstructure:"data_dir" yaml:"data_dir"`
 	} `mapstructure:"node" yaml:"node"`
@@ -28,58 +34,74 @@ type Config struct {
 	} `mapstructure:"cluster" yaml:"cluster"`
 }
 
-func LoadConfig(configName string) (*Config, error) {
-	v := viper.New()
-
-	v.SetConfigFile(configName)
-	v.SetConfigType("yaml")
-
-	if err := v.ReadInConfig(); err != nil {
-		return nil, fmt.Errorf("error reading config file, %s", err)
+func LoadConfig(configPath string) (*Config, error) {
+	if err := k.Load(file.Provider(configPath), yaml.Parser()); err != nil {
+		return nil, fmt.Errorf("failed to load config from %s: %w", configPath, err)
 	}
 
-	v.AutomaticEnv()
-	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	if err := k.Load(env.Provider(".", env.Opt{
+		TransformFunc: func(k, v string) (string, any) {
+			log.Printf("Koanf Transform: %s, %s", k, v)
+			trimmedKey := strings.TrimPrefix(k, "RAFT_")
+			parts := strings.Split(trimmedKey, "_")
+			if parts[0] != "NODE" {
+				return k, v
+			}
+
+			if parts[0] == "NODE" {
+				if len(parts) > 1 {
+					k = strings.ToLower(parts[0] + "." + strings.Join(parts[1:], "_"))
+				} else {
+					return "", fmt.Errorf("invalid format: %s", k)
+				}
+			}
+			log.Printf("After Koanf Transform: %s, %s", k, v)
+			return k, v
+		},
+	}), nil); err != nil {
+		return nil, fmt.Errorf("failed to load env: %w", err)
+	}
 
 	var config Config
-	if err := v.Unmarshal(&config); err != nil {
-		return nil, err
+	if err := k.UnmarshalWithConf("", &config, koanf.UnmarshalConf{Tag: "mapstructure"}); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
 	}
+
 	return &config, nil
 }
 
 func main() {
-	configFile := flag.String("config", "config-node1.yaml", "Config file name")
+	time.Sleep(2 * time.Second)
+	configPath := flag.String("config", "/etc/config.yaml", "Path to config file")
 	flag.Parse()
 
-	fmt.Printf("Starting server with configuration file: %s\n", *configFile)
+	fmt.Printf("Starting server with configuration file: %s\n", *configPath)
 
-	cfg, err := LoadConfig(*configFile)
+	cfg, err := LoadConfig(*configPath)
+	log.Printf("Loaded config: %+v\n", cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	port := cfg.Node.Port
 	gRpcPort := cfg.Node.GrpcPort
-	log.Printf("port: %v\n", cfg.Node.DataDir)
+
 	peerHttpAddresses := make(map[string]string)
 	peerAddresses := make(map[string]string)
 	peerIDs := make([]string, 0)
 	for _, peer := range cfg.Cluster.Peers {
-		peerHttpAddresses[peer.ID] = fmt.Sprintf("%s:%d", peer.Address, peer.Port)
 		if peer.ID == cfg.Node.ID {
 			continue
 		}
+		peerHttpAddresses[peer.ID] = fmt.Sprintf("%s:%d", peer.Address, peer.Port)
 		peerIDs = append(peerIDs, peer.ID)
-		log.Printf("%v", cfg.Cluster.Peers[0])
 		peerAddresses[peer.ID] = fmt.Sprintf("%s:%d", peer.Address, peer.GrpcPort)
 	}
-	log.Printf("Peer http addresses: %v", peerAddresses)
 	t, err := cluster.NewGRPCTransport(peerAddresses)
 	if err != nil {
 		log.Fatalf("Failed to create grpc transport: %s", err)
 	}
 	defer t.Close()
-
+	log.Printf("PEER ADDRESSES: %v", peerAddresses)
 	clk := clockwork.NewRealClock()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
